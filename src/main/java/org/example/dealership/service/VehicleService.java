e150df2bf975127a6b4922d2fb3e3751e51f40dc
dvoinescu <denis.voinescu@endava.com>
2025-12-02 11:53:05 +0200

    Created services logic

--- src/main/java/org/example/dealership/service/VehicleService.java
+++ src/main/java/org/example/dealership/service/VehicleService.java
@@ -1,7 +1,10 @@
 package org.example.dealership.service;
 
+import org.example.dealership.model.Vehicle;
 import org.example.dealership.repository.VehicleRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class VehicleService {
@@ -10,6 +13,18 @@ public class VehicleService {
 
     public VehicleService(VehicleRepository vehicleRepository) {
         this.vehicleRepository = vehicleRepository;
+    }
+
+    public List<Vehicle> findAll() {
+        return vehicleRepository.findAll();
+    }
+
+    public Vehicle findById(Integer id) {
+        return vehicleRepository.findById(id).orElse(null);
+    }
+
+    public Vehicle create(Vehicle vehicle) {
+        return vehicleRepository.save(vehicle);
     }
 
 }
