75e4710abef063760d85f0e396f00d7bf8ce06f6
dvoinescu <denis.voinescu@endava.com>
2025-12-02 11:34:20 +0200

    Created "PersonService" and "VehicleService" services

---
+++ src/main/java/org/example/dealership/service/PersonService.java
@@ -0,0 +1,15 @@
+package org.example.dealership.service;
+
+import org.example.dealership.repository.PersonRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+
+    private final PersonRepository personRepository;
+
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+}
