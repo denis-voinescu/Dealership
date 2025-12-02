0954647081da5533a2eb500fc97d81525fb1d5f1
dvoinescu <denis.voinescu@endava.com>
2025-12-02 13:19:19 +0200

    Created controller logic

--- src/main/java/org/example/dealership/controller/PersonController.java
+++ src/main/java/org/example/dealership/controller/PersonController.java
@@ -1,8 +1,12 @@
 package org.example.dealership.controller;
 
+import org.example.dealership.model.Person;
 import org.example.dealership.service.PersonService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/v1/persons")
@@ -13,5 +17,22 @@ public class PersonController {
     public PersonController(PersonService personService) {
         this.personService = personService;
     }
+
+    @GetMapping
+    public ResponseEntity<List<Person>> findAll() {
+        return ResponseEntity.ok(personService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Person> findById(@PathVariable Integer id) {
+        return ResponseEntity.ok(personService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Person> create(@RequestBody Person person) {
+        Person saved = personService.create(person);
+        return ResponseEntity.status(HttpStatus.CREATED).body(saved);
+    }
+
 
 }
